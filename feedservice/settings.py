7a18138556c1962687a31ef002e692672aa0fd79
Stefan Kögl <stefan@skoegl.net>
2011-08-08 15:17:26 +0000

    set MEDIA_ROOT setting to relative htdocs path

--- feedservice/settings.py
+++ feedservice/settings.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
-# Django settings for feedservice project.
+
+import os, os.path
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
@@ -38,7 +39,7 @@ USE_L10N = True
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/home/media/media.lawrence.com/"
-MEDIA_ROOT = ''
+MEDIA_ROOT = os.path.abspath('%s/../htdocs/media/' % os.path.dirname(__file__))
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash if there is a path component (optional in other cases).
