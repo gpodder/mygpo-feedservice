44a41555d4f2ec3eed090711f34b233085e1aebf
Stefan Kögl <stefan@skoegl.net>
2013-07-19 18:38:00 +0200

    add missing config entries

--- feedservice/settings.py
+++ feedservice/settings.py
@@ -72,6 +72,8 @@ MIDDLEWARE_CLASSES = (
 
 ROOT_URLCONF = 'feedservice.urls'
 
+WSGI_APPLICATION = 'feedservice.wsgi.application'
+
 TEMPLATE_DIRS = (
 )
 
@@ -97,6 +99,36 @@ SOUNDCLOUD_CONSUMER_KEY = os.getenv('MYGPOFS_SOUNDCLOUD_CONSUMER_KEY', '')
 FLATTR_THING = ''
 
 ALLOWED_HOSTS = filter(None, os.getenv('MYGPOFS_ALLOWED_HOSTS', '').split(';'))
+
+
+# A sample logging configuration. The only tangible logging
+# performed by this configuration is to send an email to
+# the site admins on every HTTP 500 error when DEBUG=False.
+# See http://docs.djangoproject.com/en/dev/topics/logging for
+# more details on how to customize your logging configuration.
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'filters': {
+        'require_debug_false': {
+            '()': 'django.utils.log.RequireDebugFalse'
+        }
+    },
+    'handlers': {
+        'mail_admins': {
+            'level': 'ERROR',
+            'filters': ['require_debug_false'],
+            'class': 'django.utils.log.AdminEmailHandler'
+        }
+    },
+    'loggers': {
+        'django.request': {
+            'handlers': ['mail_admins'],
+            'level': 'ERROR',
+            'propagate': True,
+        },
+    }
+}
 
 
 try:
