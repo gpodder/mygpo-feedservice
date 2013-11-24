59e8e389d86207ddea53cefddafa5aa1846f1173
Stefan Kögl <stefan@skoegl.net>
2013-11-24 20:13:11 +0100

    fix tests

--- feedservice/parse/tests.py
+++ feedservice/parse/tests.py
@@ -7,17 +7,11 @@ Replace this with more appropriate tests for your application.
 
 from django.test import TestCase
 
-from feedservice.parse.feed import Feedparser
 from feedservice.parse import parse_feed
 
 
 class SimpleTest(TestCase):
 
     def test_basic_parse(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-
         URL = 'http://feeds.feedburner.com/linuxoutlaws'
-
-        parse_feed(URL)
+        parse_feed(URL, None)
