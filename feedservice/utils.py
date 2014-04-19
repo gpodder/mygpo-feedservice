932a23d940036ff56fa1c831358da8a0e0f5672b
Stefan Kögl <stefan@skoegl.net>
2014-04-19 12:15:16 +0000

    fix SmartRedirectHandler

--- feedservice/utils.py
+++ feedservice/utils.py
@@ -171,7 +171,8 @@ class SmartRedirectHandler(urllib2.HTTPRedirectHandler):
     def http_error_301(self, req, fp, code, msg, headers):
         result = urllib2.HTTPRedirectHandler.http_error_301(
             self, req, fp, code, msg, headers)
-        result.status = code
+        if result:
+            result.status = code
         return result
 
     def http_error_302(self, req, fp, code, msg, headers):
