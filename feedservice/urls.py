3d4703f74e1388e3583d3b44017951f42c388d0b
Stefan Kögl <stefan@skoegl.net>
2016-10-07 14:01:05 +0000

    Switch to Django 1.10

--- feedservice/urls.py
+++ feedservice/urls.py
@@ -1,8 +1,9 @@
-from django.conf.urls import patterns, url
+from django.conf.urls import url
 
 from feedservice.webservice.views import ParseView, IndexView
 
-urlpatterns = patterns('',
+urlpatterns = [
  url(r'^$',               IndexView.as_view(),     name='index'),
  url(r'^parse$',          ParseView.as_view(),     name='parse'),
-)
+
+]
